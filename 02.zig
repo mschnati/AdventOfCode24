1bd66a4e9666f2b5c2aab5a896755efbda5e5cde
Marc Schnathmeier <ms730638@fh-muenster.de>
2024-12-02 22:17:42 +0100

    day 2 part 2

--- 02.zig
+++ 02.zig
@@ -4,10 +4,15 @@ const stdout = std.io.getStdOut().writer();
 pub fn main () !void {
     var file = try std.fs.cwd().openFile("inputs/input_02.txt", .{});
     defer file.close();
-    
+
     const reader = file.reader();
-    const result = try part_1(reader);
+    var result = try part_1(reader);
     try stdout.print("part 1 safe reports: {}\n", .{result});
+
+    try file.seekTo(0);
+
+    result = try part_2(reader);
+    try stdout.print("part 2 safe reports: {}\n", .{result});
 }
 
 fn part_1 (reader: anytype) !u16 {
@@ -49,5 +54,61 @@ fn is_safe(previous: i64, current: i64, increasing: bool) bool {
 }
 
 fn part_2 (reader: anytype) !u16 {
-    _ = reader;
+    var safe_reports: u16 = 0;
+    var line_buffer: [128]u8 = undefined;
+
+    while (try reader.readUntilDelimiterOrEof( &line_buffer, '\n')) |line| {
+        const trimmed_line = std.mem.trimRight(u8, line, " \n\t\r");
+        if (try is_line_safe(trimmed_line)) safe_reports += 1;
+    }
+
+    return safe_reports;
+}
+
+fn is_line_safe(line: []const u8) !bool {
+    const allocator = std.heap.page_allocator;
+
+    var tokenizer = std.mem.tokenizeSequence(u8, line, " ");
+    var levels = std.ArrayList(i64).init(allocator);
+    defer levels.deinit();
+
+    // Parse levels
+    while (tokenizer.next()) |token| {
+        try levels.append(try std.fmt.parseInt(i64, token, 10));
+    }
+
+    const levels_slice = levels.items;
+
+    if (is_sequence_safe(levels_slice)) return true;
+
+    // brute force removing each level
+    for (0..levels_slice.len) |j| {
+        var temp_levels = std.ArrayList(i64).init(allocator);
+        defer temp_levels.deinit();
+
+        for (0..levels_slice.len) |k| {
+            if (k != j) {
+                try temp_levels.append(levels_slice[k]);
+            }
+        }
+
+        if (is_sequence_safe(temp_levels.items)) return true;
+    }
+
+    return false;
+}
+
+fn is_sequence_safe(sequence: []const i64) bool {
+    if (sequence.len < 2) return true;
+    const increasing = sequence[1] > sequence[0];
+    var previous = sequence[0];
+
+    for(sequence[1..]) |current| {
+        const diff = @abs(current - previous);
+        if (diff > 3 or (increasing and current <= previous) or !increasing and current >= previous) {
+            return false;
+        }
+        previous = current;
+    }
+    return true;
 }
