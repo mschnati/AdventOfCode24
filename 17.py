3fc5a83b5f2ad04627d3430c2e9021ce7bcc274a
Marc Schnathmeier <ms730638@fh-muenster.de>
2024-12-23 09:07:09 +0100

    Day 17 Part 2 rework

--- 17.py
+++ 17.py
@@ -1,58 +1,31 @@
-inp = "inputs/input_17.txt"
-test = "test.txt"
+import re
+a, b, c, *nums = map(int, re.findall(r"\d+", open("inputs/input_17.txt").read()))
 
-with open(inp) as file:
-    for line in file:
-        part = line.partition(':')
-        if part[0][-1] == 'A':
-            A = int(part[-1].strip())
-        elif part[0][-1] == 'B':
-            B = int(part[-1].strip())
-        elif part[0][-1] == 'C':
-            C = int(part[-1].strip())
-        elif part[1] == ':':
-            program = part[-1].strip().split(',')
+def compute(A=a, B=b, C=c, pointer=0):
+    out = []
+    while pointer < len(nums):
+        num = nums[pointer+1]
+        combo = {4:A, 5:B, 6:C}.get(num, num)
+        match nums[pointer]:
+            case 0: A = A // 2**combo
+            case 1: B ^= num
+            case 2: B = combo % 8
+            case 3 if A != 0: pointer = num - 2
+            case 4: B ^= C
+            case 5: out.append(combo % 8)
+            case 6: B = A // 2**combo
+            case 7: C = A // 2**combo
+        pointer += 2
+    return out
 
-program = [int(x) for x in program]
-print(A,B,C)
-print(program)
+print(*compute(), sep=",")
 
-def combo(operand):
-    match operand:
-        case 0 | 1 | 2 | 3:
-            return operand
-        case 4:
-            return A
-        case 5:
-            return B
-        case 6:
-            return C
-        case _:
-            print('Invalid operand:', operand)
-            return None
-
-pc = 0
-while pc < len(program):
-    opcode = program[pc]
-    match opcode:
-        case 0: # adv
-            A = A // 2 ** combo(program[pc + 1])
-        case 1: # bxl
-            B = B & program[pc + 1]
-        case 2: # bst 
-            B = combo(program[pc + 1]) % 8
-        case 3: # jnz
-            if A != 0:
-                pc = program[pc + 1]
-                continue
-        case 4: # bxc
-            B = B & C
-        case 5: # out
-            print(str(combo(program[pc + 1]) % 8) + ',', end='')
-        case 6: # bdv
-            B = A // 2 ** combo(program[pc + 1])
-        case 7: # cdv
-            C = A // 2 ** combo(program[pc + 1])
-    pc += 2
-print()
-print("A:", A, "B:", B, "C:", C)
+def find_A(A, at=15):
+    if at == -1:
+        print(A)
+        exit(0)
+    while True:
+        if nums[at:] == compute(A)[at:]:
+            find_A(A, at-1)
+        A += 8**at
+find_A(8**15)
